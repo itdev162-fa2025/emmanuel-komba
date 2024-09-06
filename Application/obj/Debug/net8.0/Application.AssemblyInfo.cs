b870b4f3e060f674cf02b026151893f8ce48b555
Emmanuel <kombae@gmatc.matc.edu>
2024-09-06 13:45:29 -0500

    Completed activity 2

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc79967e17911ceab2829e3116cfd8c5b801bea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e68ce8465521b1f178eba2119ecbb05d1f1b66a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
